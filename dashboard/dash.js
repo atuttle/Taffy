d68dfd0614ed99c67d9dc560c8e484bd6d932855
Adam Tuttle <adam@fusiongrokker.com>
2015-09-27 16:33:45 -0400

    fix whitespace / spaces-vs-tabs

--- dashboard/dash.js
+++ dashboard/dash.js
@@ -1,7 +1,7 @@
 if (!String.prototype.trim) {
-  String.prototype.trim = function () {
-    return this.replace(/^[\s\xA0]+|[\s\xA0]+$/g, '');
-  };
+	String.prototype.trim = function () {
+		return this.replace(/^[\s\xA0]+|[\s\xA0]+$/g, '');
+	};
 }
 
 $(function(){
@@ -120,10 +120,10 @@ $(function(){
 				headers[ k ] = v;
 			}
 		}
-		
+
 		var basicAuthUsername = basicAuth.find("input[name=username]").val();
 		var basicAuthPassword = basicAuth.find("input[name=password]").val();
-		
+
 		if(basicAuthUsername.length && basicAuthPassword.length){
 			headers["Authorization"] =  "Basic " + Base64.encode(basicAuthUsername + ":" + basicAuthPassword);
 		}
@@ -141,14 +141,14 @@ $(function(){
 					// only do syntax highlighting if hljs is defined
 					if (typeof hljs === 'undefined') {
 						body = body.split('\n')
-								   .join('<br/>')
-								   .replace(/\s/g,'&nbsp;');
+									.join('<br/>')
+									.replace(/\s/g,'&nbsp;');
 					} else {
 						// syntax highlight json and then replace spaces at the start of each line (or after <br/>) with &nbsp;
 						body = hljs.highlight("json", body).value;
 						body = body.split('\n')
-								   .join('<br/>')
-								   .replace(/(\<br\/\>)(\s+)/g, function(match, p1, p2, offset, string){
+									.join('<br/>')
+									.replace(/(\<br\/\>)(\s+)/g, function(match, p1, p2, offset, string){
 										return [p1, p2.replace(/\s/g,'&nbsp;')].join('');
 									});
 					}
@@ -241,13 +241,13 @@ function parseHeaders(h){
 }
 
 String.prototype.supplant = function (o) {
-    return this.replace(/{(.*?)(}(?=\/)|}$)/g,
-        function (a, b) {
-        	// We need to split on the : if we're using custom token regular expressions.
-            var r = o[ b.split(':')[ 0 ] ];
-            return typeof r === 'string' || typeof r === 'number' ? r : a;
-        }
-    );
+	return this.replace(/{(.*?)(}(?=\/)|}$)/g,
+		function (a, b) {
+			// We need to split on the : if we're using custom token regular expressions.
+			var r = o[ b.split(':')[ 0 ] ];
+			return typeof r === 'string' || typeof r === 'number' ? r : a;
+		}
+	);
 };
 
 /**
@@ -260,133 +260,132 @@ var Base64 = {
 
 	// private property
 	_keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
-	
+
 	// public method for encoding
 	encode : function (input) {
-	    var output = "";
-	    var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-	    var i = 0;
-	
-	    input = Base64._utf8_encode(input);
-	
-	    while (i < input.length) {
-	
-	        chr1 = input.charCodeAt(i++);
-	        chr2 = input.charCodeAt(i++);
-	        chr3 = input.charCodeAt(i++);
-	
-	        enc1 = chr1 >> 2;
-	        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-	        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-	        enc4 = chr3 & 63;
-	
-	        if (isNaN(chr2)) {
-	            enc3 = enc4 = 64;
-	        } else if (isNaN(chr3)) {
-	            enc4 = 64;
-	        }
-	
-	        output = output +
-	        this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
-	        this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
-	
-	    }
-	
-	    return output;
+		var output = "";
+		var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+		var i = 0;
+
+		input = Base64._utf8_encode(input);
+
+		while (i < input.length) {
+
+			chr1 = input.charCodeAt(i++);
+			chr2 = input.charCodeAt(i++);
+			chr3 = input.charCodeAt(i++);
+
+			enc1 = chr1 >> 2;
+			enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+			enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+			enc4 = chr3 & 63;
+
+			if (isNaN(chr2)) {
+				enc3 = enc4 = 64;
+			} else if (isNaN(chr3)) {
+				enc4 = 64;
+			}
+
+			output = output +
+			this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+			this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+
+		}
+
+		return output;
 	},
-	
+
 	// public method for decoding
 	decode : function (input) {
-	    var output = "";
-	    var chr1, chr2, chr3;
-	    var enc1, enc2, enc3, enc4;
-	    var i = 0;
-	
-	    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-	
-	    while (i < input.length) {
-	
-	        enc1 = this._keyStr.indexOf(input.charAt(i++));
-	        enc2 = this._keyStr.indexOf(input.charAt(i++));
-	        enc3 = this._keyStr.indexOf(input.charAt(i++));
-	        enc4 = this._keyStr.indexOf(input.charAt(i++));
-	
-	        chr1 = (enc1 << 2) | (enc2 >> 4);
-	        chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-	        chr3 = ((enc3 & 3) << 6) | enc4;
-	
-	        output = output + String.fromCharCode(chr1);
-	
-	        if (enc3 != 64) {
-	            output = output + String.fromCharCode(chr2);
-	        }
-	        if (enc4 != 64) {
-	            output = output + String.fromCharCode(chr3);
-	        }
-	
-	    }
-	
-	    output = Base64._utf8_decode(output);
-	
-	    return output;
-	
+		var output = "";
+		var chr1, chr2, chr3;
+		var enc1, enc2, enc3, enc4;
+		var i = 0;
+
+		input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+		while (i < input.length) {
+
+			enc1 = this._keyStr.indexOf(input.charAt(i++));
+			enc2 = this._keyStr.indexOf(input.charAt(i++));
+			enc3 = this._keyStr.indexOf(input.charAt(i++));
+			enc4 = this._keyStr.indexOf(input.charAt(i++));
+
+			chr1 = (enc1 << 2) | (enc2 >> 4);
+			chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+			chr3 = ((enc3 & 3) << 6) | enc4;
+
+			output = output + String.fromCharCode(chr1);
+
+			if (enc3 != 64) {
+				output = output + String.fromCharCode(chr2);
+			}
+			if (enc4 != 64) {
+				output = output + String.fromCharCode(chr3);
+			}
+
+		}
+
+		output = Base64._utf8_decode(output);
+
+		return output;
 	},
-	
+
 	// private method for UTF-8 encoding
 	_utf8_encode : function (string) {
-	    string = string.replace(/\r\n/g,"\n");
-	    var utftext = "";
-	
-	    for (var n = 0; n < string.length; n++) {
-	
-	        var c = string.charCodeAt(n);
-	
-	        if (c < 128) {
-	            utftext += String.fromCharCode(c);
-	        }
-	        else if((c > 127) && (c < 2048)) {
-	            utftext += String.fromCharCode((c >> 6) | 192);
-	            utftext += String.fromCharCode((c & 63) | 128);
-	        }
-	        else {
-	            utftext += String.fromCharCode((c >> 12) | 224);
-	            utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-	            utftext += String.fromCharCode((c & 63) | 128);
-	        }
-	
-	    }
-	
-	    return utftext;
+		string = string.replace(/\r\n/g,"\n");
+		var utftext = "";
+
+		for (var n = 0; n < string.length; n++) {
+
+			var c = string.charCodeAt(n);
+
+			if (c < 128) {
+				utftext += String.fromCharCode(c);
+			}
+			else if((c > 127) && (c < 2048)) {
+				utftext += String.fromCharCode((c >> 6) | 192);
+				utftext += String.fromCharCode((c & 63) | 128);
+			}
+			else {
+				utftext += String.fromCharCode((c >> 12) | 224);
+				utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+				utftext += String.fromCharCode((c & 63) | 128);
+			}
+
+		}
+
+		return utftext;
 	},
-	
+
 	// private method for UTF-8 decoding
 	_utf8_decode : function (utftext) {
-	    var string = "";
-	    var i = 0;
-	    var c = c1 = c2 = 0;
-	
-	    while ( i < utftext.length ) {
-	
-	        c = utftext.charCodeAt(i);
-	
-	        if (c < 128) {
-	            string += String.fromCharCode(c);
-	            i++;
-	        }
-	        else if((c > 191) && (c < 224)) {
-	            c2 = utftext.charCodeAt(i+1);
-	            string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-	            i += 2;
-	        }
-	        else {
-	            c2 = utftext.charCodeAt(i+1);
-	            c3 = utftext.charCodeAt(i+2);
-	            string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-	            i += 3;
-	        }
-	
-	    }
-	
-	    return string;
+		var string = "";
+		var i = 0;
+		var c = c1 = c2 = 0;
+
+		while ( i < utftext.length ) {
+
+			c = utftext.charCodeAt(i);
+
+			if (c < 128) {
+				string += String.fromCharCode(c);
+				i++;
+			}
+			else if((c > 191) && (c < 224)) {
+				c2 = utftext.charCodeAt(i+1);
+				string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+				i += 2;
+			}
+			else {
+				c2 = utftext.charCodeAt(i+1);
+				c3 = utftext.charCodeAt(i+2);
+				string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+				i += 3;
+			}
+
+		}
+
+		return string;
 	}
 }
